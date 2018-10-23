5ae6c63b503f3e70c98380bcc3a0f73cc48fe1b4
David Ainslie <dainslie@gmail.com>
2018-10-23 22:30:38 +0100

    Top level Kafka configuration. Could be more fine-grained, but we'll save that for another day.

--- beginners-course/src/main/scala/com/backwards/config/package.scala
+++ beginners-course/src/main/scala/com/backwards/config/package.scala
@@ -1,11 +1,18 @@
 package com.backwards
 
+import org.apache.kafka.clients.CommonClientConfigs.RETRIES_CONFIG
+import org.apache.kafka.clients.producer.ProducerConfig.{ACKS_CONFIG, ENABLE_IDEMPOTENCE_CONFIG, MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION}
 import com.backwards.kafka.config.KafkaConfig
 import com.backwards.logging.Logging
 
 package object config extends ConfigOps with Logging {
   lazy val kafkaConfig: KafkaConfig = {
-    val c = load[KafkaConfig]("kafka")
+    val c = load[KafkaConfig]("kafka") +
+      (ENABLE_IDEMPOTENCE_CONFIG -> "true") +
+      (ACKS_CONFIG -> "all") +
+      (RETRIES_CONFIG -> Int.MaxValue.toString) +
+      (MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION -> "5")
+
     info(s"Kafka configuration: $c")
     c
   }
