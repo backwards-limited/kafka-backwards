a61e5855317a173f8e004efa69061965c112cb54
David Ainslie <dainslie@gmail.com>
2018-11-01 10:57:49 +0000

    Clean up before next iteration of Elasticsearch

--- backwards/src/main/scala/com/backwards/transform/Transform.scala
+++ backwards/src/main/scala/com/backwards/transform/Transform.scala
@@ -9,8 +9,8 @@ object Transform {
   object Implicits extends Implicits
 
   trait Implicits {
-    implicit class LiftTo[X[_], A](elem: X[A]) {
-      def liftTo[Y[_]](implicit transform: X ~> Y): Y[A] = transform(elem)
+    implicit class LiftTo[X[_], A](x: X[A]) {
+      def liftTo[Y[_]](implicit transform: X ~> Y): Y[A] = transform(x)
     }
 
     implicit val `future ~> IO`: ~>[Future, IO] = new (Future ~> IO) {
