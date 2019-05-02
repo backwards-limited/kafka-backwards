ead6565e3d76cf49b2867c2e72d6f30513f798f3
Plutus <ottplutus@sky.uk>
2019-05-02 18:13:03 +0100

    Kafka config as Map/Properties

--- streaming-kafka-course/src/main/scala/com/backwards/kafka/streaming/ProducerDemo.scala
+++ streaming-kafka-course/src/main/scala/com/backwards/kafka/streaming/ProducerDemo.scala
@@ -1,18 +1,24 @@
 package com.backwards.kafka.streaming
 
-import monocle.macros.syntax.lens._
-import org.apache.kafka.clients.producer.ProducerConfig.{CLIENT_ID_CONFIG, KEY_SERIALIZER_CLASS_CONFIG, VALUE_SERIALIZER_CLASS_CONFIG}
+import java.util.Properties
+import org.apache.kafka.clients.producer.ProducerConfig.CLIENT_ID_CONFIG
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
-import org.apache.kafka.common.serialization.StringSerializer
 import com.backwards.kafka.streaming.Config._
 import com.typesafe.scalalogging.LazyLogging
 
 object ProducerDemo extends App with LazyLogging {
-  val kafkaConfig = load[KafkaConfig]("kafka").lens(_.props).modify {
-    _ + (CLIENT_ID_CONFIG -> "producer-demo") + (KEY_SERIALIZER_CLASS_CONFIG -> classOf[StringSerializer].getName) + (VALUE_SERIALIZER_CLASS_CONFIG -> classOf[StringSerializer].getName)
+  // TODO - Extract to scala-backwards
+
+  implicit val map2Properties: Map[String, String] => Properties =
+    m => (new Properties /: m) {
+    case (properties, (k, v)) =>
+      properties.put(k, v)
+      properties
   }
 
-  val producer = new KafkaProducer[String, String](kafkaConfig.toProperties)
+  val kafkaProps = load[Map[String, String]]("kafka") + (CLIENT_ID_CONFIG -> "producer-demo")
+
+  val producer = new KafkaProducer[String, String](kafkaProps)
 
   val record = new ProducerRecord[String, String]("test-topic", "my-key", "my-value")
 
