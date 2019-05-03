8a1da78c50611b937c1589e4693be5665b3dc267
David Ainslie <dainslie@gmail.com>
2019-05-03 23:30:12 +0100

    Demo producer and consumer

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     ).flatten
 
   lazy val backwards: Seq[ModuleID] = {
-    val version = "1.0.16"
+    val version = "1.0.17"
 
     Seq(
       "com.github.backwards-limited" % "scala-backwards" % version % "test, it" classifier "tests",
--- streaming-kafka-course/src/main/scala/com/backwards/kafka/streaming/Demo.scala
+++ streaming-kafka-course/src/main/scala/com/backwards/kafka/streaming/Demo.scala
@@ -1,43 +1,60 @@
 package com.backwards.kafka.streaming
 
-import java.time.Duration
+import java.util.concurrent.TimeUnit
 import scala.collection.JavaConverters._
-import org.apache.kafka.clients.consumer.ConsumerRecords
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.Random
 import com.backwards.collection.MapOps._
+import com.backwards.io.Continue
 import com.backwards.kafka.streaming.Config._
+import com.backwards.time.DurationOps._
 import com.typesafe.scalalogging.LazyLogging
 
 trait Demo extends App with LazyLogging {
   val topic: String = "test-topic"
 }
 
-object ConsumerDemo extends Demo {
+object ConsumerDemo extends Demo with Continue {
   import org.apache.kafka.clients.consumer.ConsumerConfig.{CLIENT_ID_CONFIG, GROUP_ID_CONFIG}
   import org.apache.kafka.clients.consumer.KafkaConsumer
 
   val kafkaProps = load[Map[String, String]]("kafka") + (CLIENT_ID_CONFIG -> "consumer-demo") + (GROUP_ID_CONFIG -> "1")
 
   val consumer = new KafkaConsumer[String, String](kafkaProps)
-  sys.addShutdownHook(consumer.close())
   consumer subscribe asJavaCollection(Seq(topic))
 
-  while (true) {
-    val records: ConsumerRecords[String, String] = consumer poll Duration.ofSeconds(10)
+  def consume(): Unit = {
+    (consumer poll 10.seconds).iterator.asScala.foreach(println)
 
-    records.iterator().asScala.foreach(println)
+    if (continue.get) consume()
   }
+
+  checkContinue()
+  consume()
+  consumer.close()
 }
 
-object ProducerDemo extends Demo {
+object ProducerDemo extends Demo with Continue {
   import org.apache.kafka.clients.producer.ProducerConfig.CLIENT_ID_CONFIG
   import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
   val kafkaProps = load[Map[String, String]]("kafka") + (CLIENT_ID_CONFIG -> "producer-demo")
 
   val producer = new KafkaProducer[String, String](kafkaProps)
-  val record = new ProducerRecord[String, String](topic, "my-key", "my-value")
 
-  producer send record
+  def randomString(length: Int) = Stream.continually(Random.nextPrintableChar) take length mkString
 
+  def produce(sleep: Duration = 10 seconds): Unit = {
+    val record = new ProducerRecord[String, String](topic, randomString(2), randomString(10))
+    println(s"Producing: $record")
+    producer send record
+    TimeUnit.MILLISECONDS.sleep(sleep.toMillis)
+
+    if (continue.get) produce(sleep)
+  }
+
+  checkContinue()
+  produce()
   producer.close()
 }
